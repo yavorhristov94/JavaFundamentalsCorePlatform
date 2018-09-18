b6f2759e3e73f62a6ea77a64e4baabb722934d38
iavor <iavorh@Gmail>
2018-09-18 19:22:43 +0300

    Set up basic master branch with instructions

---
+++ src/com/pluralsight/Main.java
@@ -0,0 +1,17 @@
+package com.pluralsight;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// This is the base branch, from which we expand for specific lessons
+    //Do NOT initialize values in the Main method unless absolutely necessary
+    //Create a method clearly stating the function in its name, in case merging is needed
+        //List of branches:
+        /*
+        master
+
+
+
+         */
+    }
+}
