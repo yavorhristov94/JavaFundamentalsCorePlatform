213e82da0ff514227f99afd4303a37ed43b8652d
iavor <iavorh@Gmail>
2018-09-18 19:32:25 +0300

    TryWithResources branch added TODO: add helper and myautoclosable functionality

--- src/com/pluralsight/Main.java
+++ src/com/pluralsight/Main.java
@@ -1,5 +1,9 @@
 package com.pluralsight;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,7 +13,7 @@ public class Main {
         //List of branches:
         /*
         master
-
+        TryWithResources
 
 
          */
@@ -22,4 +26,64 @@ public class Main {
         System.out.println("You may proceed with your work.");
         System.out.println("Enjoy!");
     }
+
+    public static void doTryCatchFinally() {
+        char[] buff = new char[8];
+        int length;
+        Reader reader = null;
+
+        try {
+            reader = Helper.openReader("file1.txt")//method to be added later
+            while ((length = reader.read(buff)) >= 0) {
+                System.out.println("\nlength" + length);
+                for (int i = o; i < length; i++) {
+                    System.out.println(buff[i]);
+                }
+            }
+
+        } catch (IOException e) {
+            System.out.println(e.getClass().getSimpleName() + " - " + e.getMessage());
+        } finally {
+            try {
+                if (reader != null) reader.close();
+            } catch (IOException e2) {
+                System.out.println(e2.getClass().getSimpleName() + " - " + e2.getMessage());
+            }
+        }
+
+    }
+
+    //By putting the resource in the try statement, we auto-close it
+    public static void doTryWithResources(){
+        char[] buff = new char[8];
+        int length;
+        try (Reader reader = Helper.openReader("file1.txt")) {
+            while ((length = reader.read(buff)) >= 0) {
+                System.out.println("\nlength" + length);
+                for (int i = o; i < length; i++) {
+                    System.out.println(buff[i]);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e.getClass().getSimpleName() + " - " + e.getMessage());
+        }
+    }
+
+
+    public static void doTryWithResourcesMulti(){
+        char[] buff = new char[8];
+        int length;
+        //We can call several resources for auto-closing
+        try (Reader reader = Helper.openReader("file1.txt");
+             Writer writer = Helper.openWriter("file2.txt")) {
+            while ((length = reader.read(buff)) >= 0) {
+                System.out.println("\nlength" + length);
+                writer.write(buff, 0, length);//what to write, how to offset, how long to write
+            }
+        }
+        catch (IOException e) {
+            System.out.println(e.getClass().getSimpleName() + " - " + e.getMessage());
+        }
+    }
+
 }
