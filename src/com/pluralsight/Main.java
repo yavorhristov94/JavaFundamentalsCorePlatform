8c317c43b27f75a30de04a2426906f14506c098f
Yavor <iavorh@gmail.com>
2018-09-25 15:38:39 +0000

    Created branch for Persistent Properties segment

--- src/com/pluralsight/Main.java
+++ src/com/pluralsight/Main.java
@@ -21,63 +21,11 @@ public class Main {
 
         defaultMethod();
 
-
-        String filename = readArguments(args);
-        if(!Files.exists(Paths.get(filename))){System.out.println("\n File not found: "+filename);
-        return;} else showFileLines(filename);
-
-        fixFileName(args);
-        //Take argument
-        //Check for length of argument
-        //If longer than 1 word, check for quotes
-        //If no quotes, add them
     }
 
     public static void defaultMethod(){
         System.out.println("Default method working.");
         System.out.println("You may proceed with your work.");
         System.out.println("Enjoy!");
-    }
-    public static String readArguments(String[] args){
-        boolean longArg = false;
-        String fn = null;
-        String argument = null;
-        if(args.length<1){showUsage();}
-        if(args.length >= 2){longArg = true;}else return fn = args[0];
 
-
-
-
-        return fn;
-    }
-
-    public static void fixFileName(String[] args){
-        //Check if filename has multiple spaces
-            StringBuilder fixer = new StringBuilder();
-            for (String word : args){
-                fixer.append(word);
-                fixer.append(" ");
-                /* EXAMPLE
-                Args: my file name
-                It puts each of them together and a space, so the result is "my file name "
-                 */
-            }
-            fixer.setLength(fixer.length() - 1); //remove the last, unneeded space
-            System.out.println(fixer.toString());
-
-    }
-    public static void showFileLines (String filename){
-        System.out.println();
-
-        try(BufferedReader reader = Files.newBufferedReader(Paths.get(filename))){
-            String line = null;
-            while ((line = reader.readLine()) != null)
-                System.out.println(line);
-        } catch (Exception e1){System.out.println(e1.getClass().getSimpleName());}
-    }
-
-    public static void showUsage(){
-        System.out.println();
-        System.out.println("Please provide a filename to process on the command line");
-    }
 }
