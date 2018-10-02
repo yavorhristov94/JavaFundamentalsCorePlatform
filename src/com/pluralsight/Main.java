4b991b2762b231877e2fa5be06602901540c54f7
Yavor <iavorh@gmail.com>
2018-10-02 14:36:18 +0000

    Created logging branch

--- src/com/pluralsight/Main.java
+++ src/com/pluralsight/Main.java
@@ -28,61 +28,8 @@ public class Main {
 
     public static void start() {
         //keeping the main process clean
-        Properties props = new Properties();
-        props.setProperty("displayName", "Yavor Hristov");
-        String name = props.getProperty("displayName");
-        System.out.println(name);
-
-        storer();
-        xmlStorer();
-        xmlLoader();
-    }
-
-    public static void storer(){
-        Properties props = new Properties();
-        props.setProperty("accountNumber", "123-45-6789");
-        try (Writer writer = Files.newBufferedWriter(Paths.get("xyz.properties"))){
-            props.store(writer, "My comment");
-            //To store we pass in the writer that we wanna use
-            //If we dont want a comment, we pass in null
-            //This stores the FULL map of properties
-        } catch (Exception e){}
-    }
-
-    public static void storerLoader(){
-        //to add functionality as per video
-        //add 4 values manually and load it in
-        Properties props = new Properties();
-        try (Reader reader = Files.newBufferedReader(Paths.get("myapp.properties"))){
-
-        } catch (Exception e){}
-    }
-
-    public static void xmlStorer(){
-        Properties props = new Properties();
-        props.setProperty("displayName", "Yavor Hristov");
-        props.setProperty("accountNumber", "123-45-6789");
-
-        try (OutputStream out = Files.newOutputStream(Paths.get("props.xml"))){
-            props.storeToXML(out, "My comment");
-            //Note, as it's to XML we are using an output stream, not a textual writer
-        } catch (Exception e){}
 
     }
-
-    public static void xmlLoader(){
-        Properties props = new Properties();
-
-        try (InputStream in = Files.newInputStream(Paths.get("props.xml"))){
-            props.loadFromXML(in);
-            //Note, as it's to XML we are using an output stream, not a textual writer
-        } catch (Exception e){}
-
-        String val1 = props.getProperty("displayName");
-        String val2 = props.getProperty("accountNumber");
-        System.out.println(val1+" "+val2);
-    }
-
 
     public static void defaultMethod() {
         System.out.println("Default method working.");
